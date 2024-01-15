273735d0e9a20d2155d00d67d317efdeceeb4edb
perturbing <thomas.vellekoop@iohk.io>
2024-01-15 11:59:19 +0100

    make writeScriptToFile generic over plutus versions

--- plutus-gov-tool/exe/Main.hs
+++ plutus-gov-tool/exe/Main.hs
@@ -1,35 +1,37 @@
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase           #-}
+{-# LANGUAGE TypeApplications     #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE OverloadedStrings    #-}
  
 module Main where
 
-import Cardano.Api           (writeFileTextEnvelope, PlutusScriptV3, PlutusScriptV2)
-import Cardano.Api.Shelley   (File (..), PlutusScript (..), Script (..))
+import Cardano.Api           
+  ( writeFileTextEnvelope
+  , PlutusScriptVersion (..)
+  , PlutusScriptV1
+  , PlutusScriptV2
+  , PlutusScriptV3
+  , IsPlutusScriptLanguage (..))
+import Cardano.Api.Shelley   (File (..), PlutusScript (..), Script (..) )
 import PlutusTx              (CompiledCode, liftCodeDef, unsafeApplyCode)
-import ScriptsV3             (ccScriptCode, lockingScriptCode)
-import ScriptsV2             (ccScriptCodeV2, lockingScriptCodeV2, alwaysTrueMintCodeV2)
 import qualified PlutusLedgerApi.V3 as PlutusV3
 import qualified PlutusLedgerApi.V2 as PlutusV2
+import qualified PlutusLedgerApi.V1 as PlutusV1
 
-writeScriptToFileV3 :: FilePath -> PlutusScript PlutusScriptV3 -> IO ()
-writeScriptToFileV3 filePath script =
-  writeFileTextEnvelope (File filePath) Nothing script >>= \case
-    Left err -> print "error writing script"
-    Right () -> putStrLn $ "Serialized script to: " ++ filePath
+import ScriptsV3             (ccScriptCode, lockingScriptCode)
+import ScriptsV2             (ccScriptCodeV2, lockingScriptCodeV2, alwaysTrueMintCodeV2)
 
-writeScriptToFileV2 :: FilePath -> PlutusScript PlutusScriptV2 -> IO ()
-writeScriptToFileV2 filePath script =
-  writeFileTextEnvelope (File filePath) Nothing script >>= \case
-    Left err -> print "error writing script"
-    Right () -> putStrLn $ "Serialized script to: " ++ filePath
+writePlutusScriptToFile :: IsPlutusScriptLanguage lang => FilePath -> PlutusScript lang -> IO ()
+writePlutusScriptToFile filePath script = writeFileTextEnvelope (File filePath) Nothing script >>= \case
+  Left err -> print "error writing script"
+  Right () -> putStrLn $ "Serialized script to: " ++ filePath
 
--- Create file with compiled code
-writeCodeToFileV3 :: FilePath -> CompiledCode a -> IO ()
-writeCodeToFileV3 filePath = writeScriptToFileV3 filePath . PlutusScriptSerialised . PlutusV3.serialiseCompiledCode
-
--- Create file with compiled code
-writeCodeToFileV2 :: FilePath -> CompiledCode a -> IO ()
-writeCodeToFileV2 filePath = writeScriptToFileV2 filePath . PlutusScriptSerialised . PlutusV2.serialiseCompiledCode
+writeCodeToFile :: forall lang a. PlutusScriptVersion lang -> FilePath -> CompiledCode a -> IO ()
+writeCodeToFile version filePath = case version of
+  PlutusScriptV1 -> writePlutusScriptToFile @PlutusScriptV1 filePath . PlutusScriptSerialised . PlutusV1.serialiseCompiledCode
+  PlutusScriptV2 -> writePlutusScriptToFile @PlutusScriptV2 filePath . PlutusScriptSerialised . PlutusV2.serialiseCompiledCode
+  PlutusScriptV3 -> writePlutusScriptToFile @PlutusScriptV3 filePath . PlutusScriptSerialised . PlutusV3.serialiseCompiledCode
 
 ----------------------------------
 
@@ -38,14 +40,14 @@ currencySymbol = "4c4c8c78d1791f4ad31ffcb6a19d918f0393c3a1ade85885d518be65"
 
 main :: IO ()
 main = do
-  -- V3
-  writeCodeToFileV3 "./assets/V3/ccScript.plutus" ccScriptCode
-  writeCodeToFileV3 "./assets/V3/lockingScript.plutus" lockingScriptCode
+  -- V3 (we can run this once the ledger implements the V3 Context)
+  writeCodeToFile PlutusScriptV3 "./assets/V3/ccScript.plutus" ccScriptCode
+  writeCodeToFile PlutusScriptV3 "./assets/V3/lockingScript.plutus" lockingScriptCode
   -- V2
-  writeCodeToFileV2 "./assets/V2/alwaysTrueMint.plutus" alwaysTrueMintCodeV2
+  writeCodeToFile PlutusScriptV2 "./assets/V2/alwaysTrueMint.plutus" alwaysTrueMintCodeV2
   -- Apply always true minting policy to ccScript
   let appliedScriptV2 = ccScriptCodeV2 `unsafeApplyCode` liftCodeDef currencySymbol
   putStrLn $ "Applied currency symbol to script: " ++ show currencySymbol
-  writeCodeToFileV2 "./assets/V2/ccScript.plutus" appliedScriptV2
-  writeCodeToFileV2 "./assets/V2/lockingScript.plutus" lockingScriptCodeV2
+  writeCodeToFile PlutusScriptV2 "./assets/V2/ccScript.plutus" appliedScriptV2
+  writeCodeToFile PlutusScriptV2 "./assets/V2/lockingScript.plutus" lockingScriptCodeV2
   putStrLn "Done."
