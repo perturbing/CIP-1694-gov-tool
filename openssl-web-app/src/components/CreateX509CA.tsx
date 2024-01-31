f2f688fe66ff0d6a5339fd498b9b1edb1dbb57cb
perturbing <thomas.vellekoop@iohk.io>
2024-01-31 13:26:22 +0100

    add function that ca creation copies cert to clipboard

--- openssl-web-app/src/components/CreateX509CA.tsx
+++ openssl-web-app/src/components/CreateX509CA.tsx
@@ -1,4 +1,4 @@
-import { callOpenSSL } from '@/utils/utils';
+import { callOpenSSL, copyToClipboard } from '@/utils/utils';
 import React from 'react';
 
 export default function CreateX509CA({ formData, onFormDataChange, privKey }) {
@@ -6,7 +6,7 @@ export default function CreateX509CA({ formData, onFormDataChange, privKey }) {
         e.preventDefault();
         callOpenSSL('createCA', privKey, formData, 'Error creating X.509 certificate')
             .then(data => {
-                console.log(data)
+                copyToClipboard(data.result, 'X.509 certificate copied to clipboard');
             })
             .catch(error => {
                 window.alert("Error calling API: " + error);
@@ -15,6 +15,7 @@ export default function CreateX509CA({ formData, onFormDataChange, privKey }) {
 
     return (
         <div className="p-4 bg-white bg-opacity-90 rounded shadow">
+            <p className="text-lg font-medium text-gray-700">Issue a CA certificate with your connected key</p>
             <form onSubmit={handleSubmit} className="space-y-4">
 
                 {/* Country Field */}
@@ -114,7 +115,7 @@ export default function CreateX509CA({ formData, onFormDataChange, privKey }) {
                     />
                 </div>
                 
-                <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign X.509 certificate</button>
+                <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Copy X.509 certificate to clipboard</button>
             </form>
         </div>
     );
