33df3a082395fc8fdeae70f21d146f81a620d994
perturbing <thomas.vellekoop@iohk.io>
2024-02-22 14:25:23 +0100

    add plutus data json serialisation

--- plutus-gov-tool/exe/Main.hs
+++ plutus-gov-tool/exe/Main.hs
@@ -15,19 +15,26 @@ import Cardano.Api
   , IsPlutusScriptLanguage (..)
   , Script (..)
   , ScriptHash (..)
-  , hashScript)
+  , hashScript
+  , prettyPrintJSON)
 import Cardano.Api.Shelley   
   ( File (..)
   , PlutusScript (..)
   , Script (..)
-  , serialiseToRawBytes)
+  , serialiseToRawBytes
+  , fromPlutusData
+  , scriptDataToJsonDetailedSchema
+  , unsafeHashableScriptData)
 import PlutusTx               (CompiledCode, liftCodeDef, unsafeApplyCode)
+import qualified PlutusTx.Builtins  as PlutusTx
 import qualified PlutusLedgerApi.V3 as PlutusV3
 import qualified PlutusLedgerApi.V2 as PlutusV2
 import qualified PlutusLedgerApi.V1 as PlutusV1
 
 import Scripts                (alwaysTrueMintCode, lockingScriptCode, ccScriptCode)
-import qualified PlutusLedgerApi.V1 as V3
+
+import Data.Aeson             (Value)
+import qualified Data.ByteString.Char8 as BS8
 
 writePlutusScriptToFile :: IsPlutusScriptLanguage lang => FilePath -> PlutusScript lang -> IO ()
 writePlutusScriptToFile filePath script = writeFileTextEnvelope (File filePath) Nothing script >>= \case
@@ -48,10 +55,16 @@ scriptHashAlwaysTrueMint = hashScript . PlutusScript PlutusScriptV3 . PlutusScri
 alwaysTrueCurrencySymbol :: PlutusV3.CurrencySymbol
 alwaysTrueCurrencySymbol = PlutusV3.CurrencySymbol . PlutusV3.toBuiltin . serialiseToRawBytes $ scriptHashAlwaysTrueMint
 
+dataToJSON :: PlutusV3.ToData a => a -> Value
+dataToJSON = scriptDataToJsonDetailedSchema . unsafeHashableScriptData . fromPlutusData . PlutusV3.toData
+
+printDataToJSON :: PlutusV3.ToData a => a -> IO ()
+printDataToJSON = putStrLn . BS8.unpack . prettyPrintJSON . dataToJSON
+
 main :: IO ()
 main = do
   writeCodeToFile PlutusScriptV3 "./assets/V3/alwaysTrueMint.plutus" alwaysTrueMintCode
-  let ccScriptCodeAplied = ccScriptCode `unsafeApplyCode` liftCodeDef (V3.toBuiltinData alwaysTrueCurrencySymbol)
+  let ccScriptCodeAplied = ccScriptCode `unsafeApplyCode` liftCodeDef (PlutusV3.toBuiltinData alwaysTrueCurrencySymbol)
   putStrLn $ "Applied currency symbol " ++ show alwaysTrueCurrencySymbol ++ " to ccScriptCode"
   writeCodeToFile PlutusScriptV3 "./assets/V3/ccScript.plutus" ccScriptCodeAplied
   writeCodeToFile PlutusScriptV3 "./assets/V3/lockingScript.plutus" lockingScriptCode
